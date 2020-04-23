8fac68c6ab0d11e509883fd766a9845dd960b81b
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-23 17:32:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key]+= 1
+  if hash[key]
+    hash[key] + 1 
+  else 
+    hash[key]
 end
   
   # given a hash an a key as parameters, return an updated hash
